f0914799338f5bb0daefd4b2a7c930076227deca
andersonjardim24-arch <andersonjardim24@gmail.com>
2026-06-28 02:21:21 -0300

    Add configuration settings in config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    whatsapp: "5551992668360",
+
+    scriptURL: "https://script.google.com/macros/s/AKfycbzGhE4XG_2-zSt8oHmWzraL0ESJdr3jOvG1eMZbW25srzsTP1BQmdY0Y0u1DaFueP74EQ/exec",
+
+    empresa: "A&F Smart Uniformes",
+
+    freteGratis: 30
+};
